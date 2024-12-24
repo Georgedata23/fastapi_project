af7c17b10d1ee3801d1bd3bb08360b0f2c07359a
Georgedata23 <0727gm@gmail.com>
2024-12-24 10:40:16 +0300

    testing_it_2

--- app/config.py
+++ app/config.py
@@ -1,13 +1,27 @@
+from typing import Literal
+
 from pydantic import model_validator, Field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST"]
+
     DB_PORT: str
     DB_USER: str
-    DB_PASSWORD: str
+    DB_PASSWORD: int
     DB_NAME: str
     DB_HOST: str
     DATABASE_URL: str = Field(default="")
+
+    TEST_DB_PORT: str
+    TEST_DB_USER: str
+    TEST_DB_PASSWORD: int
+    TEST_DB_NAME: str
+    TEST_DB_HOST: str
+    TEST_DATABASE_URL: str = Field(default="")
+
+
+
 
     @model_validator(mode="after")
     def get_database_url(cls, values):
@@ -16,9 +30,17 @@ class Settings(BaseSettings):
         )
         return values
 
+
+    @model_validator(mode="after")
+    def get_test_database_url(cls, values):
+        values.TEST_DATABASE_URL = (
+            f"postgresql+asyncpg://{values.TEST_DB_USER}:{values.TEST_DB_PASSWORD}@{values.TEST_DB_HOST}:{values.TEST_DB_PORT}/{values.TEST_DB_NAME}"
+        )
+        return values
+
+
     class Config:
         env_file = ".env"
 
 settings = Settings()
 
-print(settings.DATABASE_URL)
--- app/database.py
+++ app/database.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.config import settings
 
 
-engine = create_async_engine(settings.DATABASE_URL)
+if settings.MODE == "TEST":
+    engine = create_async_engine(settings.TEST_DATABASE_URL)
+    print(settings.TEST_DATABASE_URL)
+elif settings.MODE == "DEV":
+    engine = create_async_engine(settings.DATABASE_URL)
+    print(settings.DATABASE_URL)
+
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_session() -> AsyncSession:
