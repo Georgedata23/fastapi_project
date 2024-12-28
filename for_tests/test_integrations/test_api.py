43ebc75a28eb12fe16e56dbb8cf065a3ec327acb
Georgedata23 <0727gm@gmail.com>
2024-12-28 17:02:13 +0300

    testing_it_7

--- for_tests/test_integrations/test_api.py
+++ for_tests/test_integrations/test_api.py
@@ -1,12 +1,13 @@
 import pytest
 import pytest_asyncio
 from httpx import AsyncClient
+from pydantic import PositiveInt
 
 
 @pytest.mark.parametrize("id, status_code, text", [(1, 200, "La-la-la"),
                                 (77, 404, "Текст с данным id не найден, используйте метод get_text для его создания!"),
                                 (5, 200, "La-la-la")])
-async def test_get_text(client: AsyncClient, id, status_code, text):
+async def test_get_text(client: AsyncClient, id: PositiveInt, status_code, text):
     resp = await client.get("/get_text", params={"id_doc": id})
     assert resp.status_code == status_code
     assert resp.json() == text
--- for_tests/test_units/test_dao.py
+++ for_tests/test_units/test_dao.py
@@ -8,4 +8,5 @@ from app.dao import DocTextDAO
 
 async def test_getter(client: AsyncClient, async_db_session: AsyncSession):
     text = await DocTextDAO.getter_text(id_doc=1, session=async_db_session)
-    assert text == "La-la"
+    print(text.body)
+    assert text.body.decode('utf-8').strip('"') == "La-la-la"
